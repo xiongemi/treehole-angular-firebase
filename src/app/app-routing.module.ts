5a1e041a87e939bb68c5e4c8bf33a0f9eae2885d
xiongemi <xiongemi@gmail.com>
2021-01-25 10:14:42 -0500

    Merge branch 'master' of https://github.com/xiongemi/treehole-angular-firebase update package.json

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -29,7 +29,8 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    initialNavigation: 'enabled'
+    initialNavigation: 'enabled',
+    relativeLinkResolution: 'legacy'
 })],
   exports: [RouterModule]
 })
