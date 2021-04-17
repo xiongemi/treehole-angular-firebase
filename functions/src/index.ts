a0fd8b8a3965c3f5dc65ddb907f87dfbedc60913
Emily Xiong <xiongemi@gmail.com>
2021-04-17 03:01:41 -0400

    update cloud functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,65 +1,75 @@
-import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import * as functions from 'firebase-functions';
 
 admin.initializeApp();
 
 const database = admin.firestore();
 
 function updateLikesDislikesCountOnPost(
-  parentDocRef: FirebaseFirestore.DocumentReference<
-    FirebaseFirestore.DocumentData
-  >,
+  change: functions.Change<FirebaseFirestore.DocumentSnapshot>,
   collectionName: 'likes' | 'dislikes'
 ) {
-  functions.logger.info(collectionName, parentDocRef);
-  return database.runTransaction(transaction => {
-    return transaction
-      .get(parentDocRef.collection(collectionName))
-      .then(collectionQuery => {
-        const count = collectionQuery.size;
-        const updateValue: any = {};
-        updateValue[collectionName + 'Count'] = count;
-        functions.logger.info(collectionName, updateValue, parentDocRef);
+  functions.logger.info('Start to update count for ' + collectionName);
 
-        return transaction.update(parentDocRef, updateValue);
-      });
+  // Get whether users have add or cancel a like/dislike
+  let increment: number;
+  if (change.after.exists && !change.before.exists) {
+    increment = 1;
+  } else if (!change.after.exists && change.before.exists) {
+    increment = -1;
+  } else {
+    return;
+  }
+
+  // Get the parent doc that contains the likesCount/dislikeCout key
+  const likesOrDislikesCollectionRef: FirebaseFirestore.CollectionReference<FirebaseFirestore.DocumentData> =
+    change.after.ref.parent;
+  const parentDocRef = likesOrDislikesCollectionRef.parent;
+  if (!parentDocRef) {
+    return;
+  }
+  functions.logger.info(increment, parentDocRef);
+
+  return database.runTransaction((transaction) => {
+    return transaction.get(parentDocRef).then((docRef) => {
+      // Get the current likesCount/dislikeCount
+      const currentCount = docRef.data()?.[collectionName + 'Count'] ?? 0;
+      // Update the doc with the count and increment
+      const updateValue: any = {};
+      updateValue[collectionName + 'Count'] = currentCount + increment;
+      functions.logger.info(
+        'Count updated for ' + collectionName,
+        updateValue,
+        parentDocRef
+      );
+
+      return transaction.update(parentDocRef, updateValue);
+    });
   });
 }
 
 exports.likeAPost = functions.firestore
   .document('posts/{postId}/likes/{likesId}')
-  .onWrite((_, context) => {
-    const postRef = database.collection('posts').doc(context.params.postId);
-    return updateLikesDislikesCountOnPost(postRef, 'likes');
+  .onWrite((change) => {
+    return updateLikesDislikesCountOnPost(change, 'likes');
   });
 
 exports.dislikeAPost = functions.firestore
   .document('posts/{postId}/dislikes/{dislikeId}')
-  .onWrite((_, context) => {
-    const postRef = database.collection('posts').doc(context.params.postId);
-    return updateLikesDislikesCountOnPost(postRef, 'dislikes');
+  .onWrite((change) => {
+    return updateLikesDislikesCountOnPost(change, 'dislikes');
   });
 
 exports.likeAComment = functions.firestore
   .document('posts/{postId}/comments/{commentId}/likes/{likesId}')
-  .onWrite((_, context) => {
-    const commentRef = database
-      .collection('posts')
-      .doc(context.params.postId)
-      .collection('comments')
-      .doc(context.params.commentId);
-    return updateLikesDislikesCountOnPost(commentRef, 'likes');
+  .onWrite((change) => {
+    return updateLikesDislikesCountOnPost(change, 'likes');
   });
 
 exports.dislikeAComment = functions.firestore
   .document('posts/{postId}/comments/{commentId}/dislikes/{dislikeId}')
-  .onWrite((_, context) => {
-    const commentRef = database
-      .collection('posts')
-      .doc(context.params.postId)
-      .collection('comments')
-      .doc(context.params.commentId);
-    return updateLikesDislikesCountOnPost(commentRef, 'dislikes');
+  .onWrite((change) => {
+    return updateLikesDislikesCountOnPost(change, 'dislikes');
   });
 
 exports.commentsCount = functions.firestore
@@ -72,10 +82,10 @@ exports.commentsCount = functions.firestore
 
     const postRef = database.collection('posts').doc(context.params.postId);
     const commentsRef = postRef.collection('comments');
-    return database.runTransaction(transaction => {
+    return database.runTransaction((transaction) => {
       return transaction
         .get(commentsRef.where('parentDocId', '==', context.params.postId))
-        .then(query => {
+        .then((query) => {
           const count = query.size;
           return transaction.update(postRef, { commentsCount: count });
         });
